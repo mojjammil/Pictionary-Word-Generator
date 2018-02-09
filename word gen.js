70223fe8d95f98726abbc996fd0acc93479daff9
Mojjammil Khandker <mojjammil@gmail.com>
2018-02-09 10:47:46 +0200

    Final

---
+++ word gen.js
@@ -0,0 +1,5 @@
+var words = ["treasure", "iPad", "password", "music", "tutor", "exercise", "macaroni", "school", "cake", "html", "dancing", "running", "sleeping", "eating", "progamming", "singing", "laptop", "party", "metal", "hunter", "mouse", "hockey", "football", "superman", "batman", "clown", "nokia", "pancake", "bread", "chocolate", "ice cream", "cheese"]
+function randomWords() {
+  var randomWord = Math.floor(Math.random() * (words.length));
+  document.getElementById("word").innerHTML = words[randomWord];
+}
